705abc96fed0a3c9bd5d4975ec81bc5ca91da9a4
KMM <vtmoorek1@gmail.com>
2020-01-24 09:54:30 -0500

    initial page menu

--- components/PageMenu.js
+++ components/PageMenu.js
@@ -12,17 +12,26 @@ export default class PageMenu extends Component {
         this.getPageItems = this.getPageItems.bind(this);
     }
 
-    getPageItems()
-    {
-        return [<li><a href="">Main Page</a></li>,<li><a href="">Second Page</a></li>];
+    getPageItems() {
+        return [<li key="1"><a href="">Main Page</a></li>, <li key="2"><a href="">Second Page</a></li>];
     }
 
     render() {
 
-        return <div id="pageMenu">
-            <ul id="pageList">
-                {this.getPageItems()}
-            </ul>
+        return <div id="pageMenu" className="bg-dark">
+            <table id="pageTable">
+                <tbody>
+                    <tr id="pageTableHeaderRow">
+                        <td id="pageTableHeaderCell"x><button id="addPageBtn" type="button" className="btn btn-primary"><i className="fa fa-plus"></i></button></td>
+                    </tr>
+                    <tr>
+                        <td><a>Funny Page</a></td>
+                    </tr>
+                    <tr>
+                        <td><a>News</a></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>;
     }
 
